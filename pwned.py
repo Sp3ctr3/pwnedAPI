91db0925af2452d592e5c96a74a7a401ed80df95
Yashin Mehaboobe <yashinm92@gmail.com>
2013-12-09 20:23:47 +0530

    Fixed response big by changing indentation.

--- pwned.py
+++ pwned.py
@@ -7,15 +7,15 @@ def check(emails):
     dic={}
     for email in emails:
         em=email.replace("@","%40")
-    try:
-        raw=urlopen('http://haveibeenpwned.com/api/breachedaccount/'+em)
-        data=raw.read()
-        info=json.loads(data)
-        for i in range(len(info)):
-            info[i]=info[i].encode('ascii')
-        dic.update({email:info})
-    except:
-        dic.update({email:'None'})
+        try:
+            raw=urlopen('http://haveibeenpwned.com/api/breachedaccount/'+em)
+            data=raw.read()
+            info=json.loads(data)
+            for i in range(len(info)):
+                info[i]=info[i].encode('ascii')
+            dic.update({email:info})
+        except:
+            dic.update({email:'None'})
     return dic
 if __name__=="__main__":
 	length = len(sys.argv)-1
